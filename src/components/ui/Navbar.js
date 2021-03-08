189d48f647d0e6f747229171ffe27755b872e279
Gregory Palacios <gregpalacios26@gmail.com>
2021-03-08 16:41:48 -0500

    React heroes primer commit

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,78 @@
+import React, { useContext } from 'react';
+import { Link, NavLink, useHistory } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
+
+export const Navbar = () => {
+
+    const { user: { name }, dispatch } = useContext(AuthContext);
+    const history = useHistory();
+
+    const handleLogout = () => {
+
+        dispatch({
+            type: types.logout
+        });
+
+        history.replace('/login');
+    }
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link
+                    className="navbar-brand"
+                    to="/"
+                >
+                    Asociaciones
+                </Link>
+
+                <div className="navbar-collapse">
+                    <div className="navbar-nav">
+                        <NavLink
+                            activeClassName="active"
+                            className="nav-item nav-link"
+                            exact
+                            to="/marvel"
+                        >
+                            Marvel
+                        </NavLink>
+
+                        <NavLink
+                            activeClassName="active"
+                            className="nav-item nav-link"
+                            exact
+                            to="/dc"
+                        >
+                            DC
+                        </NavLink>
+
+                        <NavLink
+                            activeClassName="active"
+                            className="nav-item nav-link"
+                            exact
+                            to="/search"
+                        >
+                            Search
+                        </NavLink>
+                    </div>
+                </div>
+
+                <div className="d-flex">
+                    <ul className="navbar-nav ml-auto">
+                        <span className="nav-item nav-link text-info">
+                            {name}
+                        </span>
+
+                        <button
+                            className="nav-item nav-link btn"
+                            onClick={handleLogout}
+                        >
+                            Logout
+                        </button>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
